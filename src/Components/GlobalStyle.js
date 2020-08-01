f2c41b86330972c14b6d92075d4788b79a4db3b8
awesome <dado2472@gmail.com>
2020-08-01 14:38:44 +0900

    styles

--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -3,6 +3,25 @@ import { reset } from "styled-reset";
 
 const globalStyle = createGlobalStyle`
   ${reset};
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    width: 100%;
+    height: 200vh;
+    background-color: rgb(20, 20, 20);
+    color: white;
+    padding-top: 65px;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  
 `;
 
 export default globalStyle;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,54 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
-export default () => <h1>Hello</h1>;
+const Header = styled.header`
+  width: 100%;
+  height: 65px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(20, 20, 20, 0.8);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+`;
+
+const Nav = styled.ul`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  font-size: 18px;
+`;
+
+const Item = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: solid 3px
+    ${(props) => (props.current ? "#e50814" : "transparent")};
+  transition: border-bottom ease-in-out 0.3s;
+`;
+
+const SLink = styled(Link)`
+  height: 100%;
+  width: 100%;
+  padding: 20px 20px;
+  margin: 0 10px;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <Nav>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Home</SLink>
+      </Item>
+      <Item current={pathname === "/TV"}>
+        <SLink to="/TV">TV</SLink>
+      </Item>
+      <Item current={pathname === "/Search"}>
+        <SLink to="/Search">Search</SLink>
+      </Item>
+    </Nav>
+  </Header>
+));
